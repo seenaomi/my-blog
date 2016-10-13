ff76a0681d689454c6c524f92d063fb3f5f6cb84
Naomi <naomi.see@seenaomi.net>
2016-10-13 14:04:45 -0500

    Still working through bug regarding comments

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 		return self.title
 
 	def approved_comment(self):
-		return self.comments.filter(approved_comment=True)	
+		return self.comment.filter(approved_comment=True)	
 
 
 class Comment(models.Model):
