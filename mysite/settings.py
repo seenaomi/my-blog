592e78a867c649d3027573082c708464264cf17d
Naomi <naomi.see@seenaomi.net>
2016-10-14 15:20:24 -0500

    prepping for Heroku deploy hopefully its smooth this time

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'seenaomi',
+        'PASSWORD': 'goodgood',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
