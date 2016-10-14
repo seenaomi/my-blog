6591b1e3dbd06c634e381d1bca8381cc2d44af10
Naomi <naomi.see@seenaomi.net>
2016-10-14 13:45:58 -0500

    setting things up to deploy to Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Registration/login 
 
 LOGIN_REDIRECT_URL = '/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
