1cf3e0d44a51463c044ad4d9efa7482f1b6b97e8
Naomi <naomi.see@seenaomi.net>
2016-10-17 13:28:29 -0500

    adding back the postgresql db info into the app to deploy to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'seenaomi',
+        'PASSWORD': 'goodgood',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
